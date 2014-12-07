ce257ba20c02b93f6f04233b0129961e0341f936
Anders Pearson <anders@columbia.edu>
2014-12-07 15:45:26 -0500

    favicon requests are annoying

--- cask.go
+++ cask.go
@@ -11,7 +11,12 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
 }
 
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	// just ignore this crap
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("CASK_PORT"), nil))
 }
